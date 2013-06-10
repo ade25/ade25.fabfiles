69eaf7a82cbed66892be61b97d3bbef58b8efe42
Christoph Boehner <cb@vorwaerts-werbung.de>
2013-06-10 18:26:56 +0200

    Add database maintenance fabric scripts

---
+++ ade25/fabfiles/project/db.py
@@ -0,0 +1,34 @@
+from fabric.api import env
+from fabric.api import task
+from fabric.api import local
+from fabric.api import sudo
+from fabric.api import get
+from fabric.api import exists
+from fabric.api import cd
+from fabric.api import confirm
+
+
+@task
+def download_data():
+    """ DB filestorage and blobstorage download """
+
+    if not env.get('confirm'):
+        confirm("This will destroy all current Zope data on your local "
+                " machine. Are you sure you want to continue?")
+
+    with cd(env.code_root):
+        # remove temporary Data.fs file from previous downloads
+        if exists('/tmp/Data.fs', use_sudo=True):
+            sudo('rm -rf /tmp/Data.fs')
+        # download Data.fs from server
+        sudo('rsync -a filestorage/Data.fs /tmp/Data.fs')
+        get('/tmp/Data.fs', '%(code_root)s/var/filestorage/Data.fs' % env)
+        # remove temporary blobs from previous downloads
+        if exists('/tmp/blobstorage', use_sudo=True):
+            sudo('rm -rf /tmp/blobstorage')
+        # download blobs from server
+        sudo('rsync -a blobstorage /tmp/')
+        sudo('chown -R %(user)s /tmp/blobstorage' % env)
+        tmp_loc = '%(code_user)s@%(hostname)s:/tmp/blobstorage' % env
+        path_bs = '%(code_root)s/var/' % env
+        local('rsync -aPz %s %s' % (tmp_loc, path_bs))
